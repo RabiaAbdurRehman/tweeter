5463218278c396c3bca7568d9a1b59dee271020a
Rabia Abdur Rehman <rabia.abdur.rehman@gmail.com>
2023-07-19 16:33:58 -0600

    I have added functionality to my tweeter to make it dynamic and tweeting a message which can be displayed on the page with proper styles.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,91 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function () {
+  $("#error-messages-for-empty").hide();
+  $("#error-messages-for-exceeding").hide();
+  /**this function gets a tweet and create tweet elements. */
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+
+  const createTweetElement = function (tweet) {
+    let $tweet = ` <article class="tweet">
+    <header>
+
+      <div class="intro">
+        <img width="80px" src="${tweet.user.avatars}">
+        <div class="name1">${tweet.user.name}</div>
+      </div>
+      <div class="email">${tweet.user.handle}</div>
+    </header>
+    <p>${escape(tweet.content.text)}</p>
+    <footer>
+      <span class="tweet-age">
+      ${timeago.format(
+        tweet.created_at + 1 * 60 * 60 * 1000 + 3 * 60 * 1000 + 42 * 1000
+      )}
+        </span>
+      <div>
+        <i class="fa-solid fa-flag"></i>
+        <i class="fa-solid fa-retweet"></i>
+        <i class="fa-solid fa-heart"></i>
+      </div>
+    </footer>
+    </article>`;
+    return $tweet;
+  };
+  /** this function take an array of objects and render them to the DOM*/
+  const renderTweets = function (tweets) {
+    // loops through tweets
+    tweets.map((tweet) => {
+      //calls createTweetElement for each tweet
+      const $tweet = createTweetElement(tweet);
+
+      // takes return value and appends it to the tweets container
+      $("#tweets-container").prepend($tweet);
+    });
+  };
+  //adding an Event handlers
+  $("#new-tweet-form").on("submit", function (event) {
+    event.preventDefault();
+    $("#error-messages-for-empty").hide();
+    $("#error-messages-for-exceeding").hide();
+    let tweetContents = $(this).serialize();
+    //console.log("String  ", tweetContents);
+    //To create a variable so I can use to apply my conditions.
+    let txt = $("#tweet-text").val();
+
+    if (txt.length === 0) {
+      $("#error-messages-for-empty").show();
+      return;
+    }
+    //$("#error-messages-for-empty").hide();
+    if (txt.length > 140) {
+      $("#error-messages-for-exceeding").show();
+      return;
+    }
+    $.post("/tweets", tweetContents, function (data) {
+      //This line is to clear the form after submission.
+      $("#tweet-text").val("");
+      // this line is to reset the counter.
+      $(".counter").text(140);
+      // location.reload(true);
+      loadTweets();
+    });
+  });
+  /*this function is responsible to fetch tweets by requesting
+    through jquery*/
+  const loadTweets = function () {
+    $.ajax({
+      url: "/tweets",
+      dataType: "json",
+      success: function (tweet) {
+        renderTweets(tweet);
+      },
+    });
+  };
+  loadTweets();
+});
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,16 +1,19 @@
 //document object helps JS to access and modify my elements in
 //my website.
-$(document).ready(function() {
-    //$( "p" ).text( "The DOM is now loaded and can be manipulated." );
-   let counter = 140;
-   $("#tweet-text").keyup(function(){
+$(document).ready(function () {
+  //$( "p" ).text( "The DOM is now loaded and can be manipulated." );
+
+  $("#tweet-text").keyup(function () {
     let currentLength = $(this).val().length;
     let counter = 140;
     let textLength = counter - currentLength;
-    $('.counter').text(textLength);
-   });
-
-
+    if (textLength < 0) {
+      $(".counter").css("color", "red");
+    } else {
+      $(".counter").css("color", "#545149");
+    }
+    $(".counter").text(textLength);
+  });
 });
 //register an event handler to the
 // textarea element for the form
